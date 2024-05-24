4317701ff68fb3a8e1fbd98ce80fb669bd102f49
Yigithan Yigit <yigithanyigitdevel@gmail.com>
2024-05-24 14:39:58 +0300

    Change GreenMap logo color

--- src/components/HudComponent.jsx
+++ src/components/HudComponent.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 function HUD() {
   return (
     <div className="flex justify-center">
-      <div className="absolute p-4 z-10 ">
-        <h2 className="text-3xl font-bold">GreenMap</h2>
+      <div className="absolute p-4 z-10 flex">
+        <h2 className="text-3xl font-bold text-custom-green">Green</h2>
+        <h2 className="text-3xl font-bold text-custom">Map</h2>
       </div>
     </div>
   );
