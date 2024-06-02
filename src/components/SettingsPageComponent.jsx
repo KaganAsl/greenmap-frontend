4521350f99a5a4f0338ef3930d501cfa3d1a71f3
KaganAsl <kagan.aslancan@gmail.com>
2024-06-02 20:35:23 +0300

    Confirm password added

--- src/components/SettingsPageComponent.jsx
+++ src/components/SettingsPageComponent.jsx
@@ -10,6 +10,7 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
     const [error, setError] = useState('');
     const [info, setInfo] = useState('');
     const [showModal, setShowModal] = useState(false);  // State for modal visibility
+    const [confirmPassword, setConfirmPassword] = useState('');  // State for confirm password
 
     const { loggedIn, setLoggedIn } = useLoggedIn();
 
@@ -45,6 +46,15 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
     };
 
     const handleUpdateAccount = () => {
+        if (!password || !confirmPassword) {
+            setError("Please fill in the required fields");
+            setInfo("");
+            return;
+        } else if (password !== confirmPassword) {
+            setError("Passwords do not match");
+            setInfo("");
+            return;
+        }
         instance.post("/user/updateUser", {
             username: username,
             email: email,
@@ -124,7 +134,7 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
                 <h2 className="mt-0 mb-5 text-center text-xl leading-9 tracking-tight text-gray-900">
                     Settings
                 </h2>
-                <div className='mb-4'>
+                <div className='mb-4 bg-gray-100'>
                     <div className='flex items-center border rounded shadow appearance-none w-full'>
                     <i className='p-1 ml-2'>
              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
@@ -133,7 +143,7 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
                             Username
                         </label>
                         <input
-                            className='shadow appearance-none w-full py-2 px-5 ml-0 text-gray-700 leading-tight focus:outline-none'
+                            className='shadow appearance-none w-full py-2 px-5 ml-0 text-gray-700 leading-tight focus:outline-none bg-gray-100'
                             id='username'
                             type='text'
                             value={username}
@@ -142,7 +152,7 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
                         />
                     </div>
                 </div>
-                <div className='mb-4'>
+                <div className='mb-4 bg-gray-100'>
                     <div className='flex items-center border rounded shadow appearance-none w-full'>
                     <i className='p-1 ml-2'>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" /></svg>       </i>
@@ -151,7 +161,7 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
                             Email
                         </label>
                         <input
-                            className='shadow appearance-none w-full py-2 px-5 ml-11 text-gray-700 leading-tight focus:outline-none'
+                            className='shadow appearance-none w-full py-2 px-5 ml-11 text-gray-700 leading-tight focus:outline-none bg-gray-100'
                             id='email'
                             type='email'
                             value={email}
@@ -178,6 +188,24 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
                         />
                     </div>
                 </div>
+                <div className='mb-4'>
+                    <div className='flex items-center border rounded shadow appearance-none w-full'>
+                    <i className='p-1 ml-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z" /></svg>
+                        </i>
+                        <label className='block text-gray-700 text-sm font-semibold mr-4 ml-2' htmlFor='confirmPassword'>
+                            Password
+                        </label>
+                        <input
+                            className='shadow appearance-none w-full py-2 px-5 ml-1 text-gray-700 leading-tight focus:outline-none'
+                            id='confirmPassword'
+                            type='password'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            placeholder="Confirm Password"
+                        />
+                    </div>
+                </div>
                 {info && <p className='text-green-500'>{info}</p>}
                 {error && <p className='text-red-500'>{error}</p>}
                 <div className='flex items-center justify-center'>
@@ -186,7 +214,7 @@ const SettingsPageComponent = ({ setSettingsButton }) => {
                         type='button'
                         onClick={handleUpdateAccount}
                     >
-                        Update Credentials
+                        Update Password
                     </button>
                 </div>
                 <div className='flex items-center justify-center'>
